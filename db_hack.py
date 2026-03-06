fdbd65638d76ae394f8b7e0abe26adc5174ae084
alinarogova123-arch <alinarogova123@gmail.com>
2026-03-06 13:20:41 +0300

    Add files via upload

---
+++ db_hack.py
@@ -0,0 +1,58 @@
+import random
+from datacenter.models import Chastisement
+from datacenter.models import Schoolkid
+from datacenter.models import Mark
+from datacenter.models import Commendation
+from datacenter.models import Lesson
+
+
+CHILD_NAME = "Фролов Иван Григорьевич"
+COMMENDATIONS = [
+	"Молодец!",
+	"Отлично!",
+	"Хорошо!",
+	"Гораздо лучше, чем я ожидал!",
+	"Ты меня приятно удивил!",
+	"Великолепно!",
+	"Прекрасно!",
+	"Ты меня очень обрадовал!",
+	"Именно этого я давно ждал от тебя!",
+	"Сказано здорово – просто и ясно!",
+	"Ты, как всегда, точен!",
+	"Очень хороший ответ!",
+	"Талантливо!",
+	"Ты сегодня прыгнул выше головы!",
+	"Я поражен!",
+	"Уже существенно лучше!",
+	"Потрясающе!",
+	"Замечательно!",
+	"Прекрасное начало!",
+	"Так держать!",
+	"Ты на верном пути!",
+	"Здорово!",
+	"Это как раз то, что нужно!",
+	"Я тобой горжусь!",
+	"С каждым разом у тебя получается всё лучше!",
+	"Мы с тобой не зря поработали!",
+	"Я вижу, как ты стараешься!",
+	"Ты растешь над собой!",
+	"Ты многое сделал, я это вижу!",
+	"Теперь у тебя точно все получится!"
+]
+
+
+def fix_marks(child=CHILD_NAME):
+	for mark in Mark.objects.filter(schoolkid__full_name=child, points__in=[2,3]):
+		mark.points = 5
+		mark.save()
+
+
+def remove_chastisements(child=CHILD_NAME):
+	Chastisement.objects.filter(schoolkid__full_name=child).delete()
+
+
+def create_commendation(subject_title, child=CHILD_NAME):
+	commendation = random.choice(COMMENDATIONS)
+	schoolkid = Schoolkid.objects.get(full_name=child)
+	lession = Lesson.objects.filter(year_of_study=6, group_letter="А", subject__title=subject_title.capitalize()).order_by("?").first()
+	Commendation.objects.create(text=commendation, created=lession.date, schoolkid=schoolkid, subject=lession.subject, teacher=lession.teacher)
