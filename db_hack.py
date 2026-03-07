9f8d92d5d6cf38787cb79c045122d4c21b3a2ba8
alinarogova123-arch <alinarogova123@gmail.com>
2026-03-07 17:25:46 +0300

    Update db_hack.py

--- db_hack.py
+++ db_hack.py
@@ -6,7 +6,6 @@ from datacenter.models import Commendation
 from datacenter.models import Lesson
 
 
-CHILD_NAME = "Фролов Иван Григорьевич"
 COMMENDATIONS = [
 	"Молодец!",
 	"Отлично!",
@@ -41,18 +40,19 @@ COMMENDATIONS = [
 ]
 
 
-def fix_marks(child=CHILD_NAME):
+def fix_marks(child="Фролов Иван Григорьевич"):
 	for mark in Mark.objects.filter(schoolkid__full_name=child, points__in=[2,3]):
 		mark.points = 5
 		mark.save()
 
 
-def remove_chastisements(child=CHILD_NAME):
+def remove_chastisements(child="Фролов Иван Григорьевич"):
 	Chastisement.objects.filter(schoolkid__full_name=child).delete()
 
 
-def create_commendation(subject_title, child=CHILD_NAME):
+def create_commendation(subject_title, child="Фролов Иван Григорьевич"):
 	commendation = random.choice(COMMENDATIONS)
 	schoolkid = Schoolkid.objects.get(full_name=child)
 	lession = Lesson.objects.filter(year_of_study=6, group_letter="А", subject__title=subject_title.capitalize()).order_by("?").first()
 	Commendation.objects.create(text=commendation, created=lession.date, schoolkid=schoolkid, subject=lession.subject, teacher=lession.teacher)
+
